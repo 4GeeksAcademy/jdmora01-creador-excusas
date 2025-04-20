4aa93d5e6851a55a56a5c50add1e5c59db70a717
Jose Mora <j.donismora@gmail.com>
2025-04-20 20:48:07 +0000

    generador-excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let quien = ['El perro', 'my abuela', 'el cartero', 'mi vecino', 'mi hermano', 'mi hermana', 'mi amigo'];
+let accion = ['se cayo', 'corrio', 'choco', 'peleo' ,'se cayo', 'me grito', 'me llamo'];
+let que = ['mi t.v', 'el carro', 'la casa', 'el perro', 'la computadora', 'el gato', 'el libro'];
+let cuando = ['en la tarde', 'antes de dormir', 'despues de comer', 'urante mi siesta', 'jugaba futbol', 'estaba en la escuela', 'cuando estaba en la casa']
+
+
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+  let excuse = randomElement(quien) + " " + randomElement(accion) + " " + randomElement(que) + " " + randomElement(cuando);
+  console.log(excuse);
+  document.getElementById("excuse").innerText = excuse 
+
+
 };
